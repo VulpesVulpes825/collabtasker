c19b49c188161d4e4cc729c9ef7655f1668671ea
Vulpesvulpes825 <vulpesvulpes825@gmail.com>
2024-03-12 14:25:48 -0700

    feat: Add initial backend files

---
+++ backend/src/main/kotlin/org/junyinchen/collabtaskerbackend/CollabtaskerBackendApplication.kt
@@ -0,0 +1,11 @@
+package org.junyinchen.collabtaskerbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CollabtaskerBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<CollabtaskerBackendApplication>(*args)
+}
---
+++ backend/src/test/kotlin/org/junyinchen/collabtaskerbackend/CollabtaskerBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.junyinchen.collabtaskerbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CollabtaskerBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
