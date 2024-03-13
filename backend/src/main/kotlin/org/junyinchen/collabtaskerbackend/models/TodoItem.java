9d23f7a2a8415bc0c6f6157c373cb21adaa6793b
VulpesVulpes825 <vulpesvulpes825@gmail.com>
2024-03-13 11:21:26 -0700

    feat(models): Add User Privileges and Roles (#6)

--- backend/src/main/kotlin/org/junyinchen/collabtaskerbackend/models/TodoItem.java
+++ backend/src/main/kotlin/org/junyinchen/collabtaskerbackend/models/TodoItem.java
@@ -1,12 +1,15 @@
 package org.junyinchen.collabtaskerbackend.models;
 
 import jakarta.persistence.*;
-import java.time.Instant;
-import java.util.UUID;
+
 import lombok.*;
 import lombok.extern.java.Log;
+
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Log
 @Getter
@@ -16,25 +19,25 @@ import org.hibernate.annotations.UpdateTimestamp;
 @Entity
 @Table
 public class TodoItem {
-  /** The id of the item in UUID */
-  @Id
-  @GeneratedValue(strategy = GenerationType.UUID)
-  private UUID id;
+    /** The id of the item in UUID */
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
-  /** The tile of the item */
-  @NonNull private String title = "";
+    /** The tile of the item */
+    @NonNull private String title = "";
 
-  /** The context user put in the item */
-  @NonNull private String content = "";
+    /** The context user put in the item */
+    @NonNull private String content = "";
 
-  /** Auto generated created time */
-  @CreationTimestamp private Instant createdOn;
+    /** Auto generated created time */
+    @CreationTimestamp private Instant createdOn;
 
-  /** Auto generated updated time */
-  @UpdateTimestamp private Instant lastUpdatedOn;
+    /** Auto generated updated time */
+    @UpdateTimestamp private Instant lastUpdatedOn;
 
-  /** The user set due date of the item, optional */
-  @Basic
-  @Temporal(TemporalType.TIMESTAMP)
-  private java.util.Date utilTimestamp;
+    /** The user set due date of the item, optional */
+    @Basic
+    @Temporal(TemporalType.TIMESTAMP)
+    private java.util.Date utilTimestamp;
 }
