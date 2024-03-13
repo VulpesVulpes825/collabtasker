069aef2e1442ad2cf6a624e2f01d6b25016eab45
Vulpesvulpes825 <vulpesvulpes825@gmail.com>
2024-03-13 10:50:40 -0700

    style(TodoItem.java): Format comment to match google java style

--- backend/src/main/kotlin/org/junyinchen/collabtaskerbackend/models/TodoItem.java
+++ backend/src/main/kotlin/org/junyinchen/collabtaskerbackend/models/TodoItem.java
@@ -8,9 +8,7 @@ import lombok.extern.java.Log;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-/**
-*
-*/
+
 @Log
 @Getter
 @Setter
@@ -19,41 +17,25 @@ import org.hibernate.annotations.UpdateTimestamp;
 @Entity
 @Table
 public class TodoItem {
-    /**
-    * The id of the item in UUID
-    */
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+  /** The id of the item in UUID */
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
 
-    /**
-    * The tile of the item
-    */
-    @NonNull
-    private String title = "";
+  /** The tile of the item */
+  @NonNull private String title = "";
 
-    /**
-    * The context user put in the item
-    */
-    @NonNull
-    private String content = "";
+  /** The context user put in the item */
+  @NonNull private String content = "";
 
-    /**
-    * Auto generated created time
-    */
-    @CreationTimestamp
-    private Instant createdOn;
+  /** Auto generated created time */
+  @CreationTimestamp private Instant createdOn;
 
-    /**
-    * Auto generated updated time
-    */
-    @UpdateTimestamp
-    private Instant lastUpdatedOn;
+  /** Auto generated updated time */
+  @UpdateTimestamp private Instant lastUpdatedOn;
 
-    /**
-    * The user set due date of the item, optional
-    */
-    @Basic
-    @Temporal(TemporalType.TIMESTAMP)
-    private java.util.Date utilTimestamp;
+  /** The user set due date of the item, optional */
+  @Basic
+  @Temporal(TemporalType.TIMESTAMP)
+  private java.util.Date utilTimestamp;
 }
