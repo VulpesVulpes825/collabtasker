75635c80613e9a5da6290d120a379592a631be7d
Vulpesvulpes825 <vulpesvulpes825@gmail.com>
2024-03-15 15:08:22 -0700

    feat(TodoBoardController): return board's name and todo items

--- backend/src/main/kotlin/org/junyinchen/collabtaskerbackend/models/ItemResponse.java
+++ backend/src/main/kotlin/org/junyinchen/collabtaskerbackend/models/ItemResponse.java
@@ -6,13 +6,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ItemResponse {
-    private long id;
+    private UUID id;
     private String title;
     private String content;
     private Date createdOn;
