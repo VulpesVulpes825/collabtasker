2f9517d4ed0276c07e038d949dd58f48f20d1a30
Vulpesvulpes825 <vulpesvulpes825@gmail.com>
2024-03-15 09:30:56 -0700

    feat(main): add react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>Hello World.</h1>,
+    },
+    {
+        path: "/test",
+        element: <App/>,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
+            <RouterProvider router={router} />
+        </DevSupport>
+    </React.StrictMode>,
 )
