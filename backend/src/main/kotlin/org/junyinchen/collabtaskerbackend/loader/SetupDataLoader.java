ff395fb1cd7d35f6f67ae8290f6e1c7bc44ab70b
Vulpesvulpes825 <vulpesvulpes825@gmail.com>
2024-03-15 15:41:53 -0700

    feat(SetupDataLoader): load one item to inital Todo Board

--- backend/src/main/kotlin/org/junyinchen/collabtaskerbackend/loader/SetupDataLoader.java
+++ backend/src/main/kotlin/org/junyinchen/collabtaskerbackend/loader/SetupDataLoader.java
@@ -7,12 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.junyinchen.collabtaskerbackend.models.Role;
 import org.junyinchen.collabtaskerbackend.models.TodoBoard;
+import org.junyinchen.collabtaskerbackend.models.TodoItem;
 import org.junyinchen.collabtaskerbackend.models.User;
-import org.junyinchen.collabtaskerbackend.repositories.BoardRepository;
-import org.junyinchen.collabtaskerbackend.repositories.PrivilegeRepository;
-import org.junyinchen.collabtaskerbackend.repositories.RoleRepository;
-import org.junyinchen.collabtaskerbackend.repositories.UserRepository;
-import org.junyinchen.collabtaskerbackend.services.BoardService;
+import org.junyinchen.collabtaskerbackend.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -30,7 +27,7 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
     private final PrivilegeRepository privilegeRepository;
     private final PasswordEncoder passwordEncoder;
     private final BoardRepository boardRepository;
-    private final BoardService boardService;
+    private final ItemRepository itemRepository;
     boolean alreadySetup = false;
 
     @Autowired
@@ -40,13 +37,13 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
             PrivilegeRepository privilegeRepository,
             PasswordEncoder passwordEncoder,
             BoardRepository boardRepository,
-            BoardService boardService) {
+            ItemRepository itemRepository) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.privilegeRepository = privilegeRepository;
         this.passwordEncoder = passwordEncoder;
         this.boardRepository = boardRepository;
-        this.boardService = boardService;
+        this.itemRepository = itemRepository;
     }
 
     @Override
@@ -62,9 +59,19 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
                 Role.builder().name("ROLE_BOARD_1").privileges(Collections.emptyList()).build();
         roleRepository.save(userRole);
         roleRepository.save(boardRole);
-        log.info("Populate Database with Board");
+        log.info("Populate Database with Todo Board");
         var board = TodoBoard.builder().title("Test's Todo Board").role(boardRole).build();
         boardRepository.save(board);
+        log.info("Populate Database with Todo Item");
+        var item =
+                TodoItem.builder()
+                        .title("A sample Todo Item")
+                        .content("More details of the Todo Item")
+                        .board(board)
+                        .build();
+        itemRepository.save(item);
+        log.info("Link Todo Item with Todo Board");
+        board.setItems(Collections.singletonList(item));
         log.info("Populate Database with User");
         var user =
                 User.builder()
