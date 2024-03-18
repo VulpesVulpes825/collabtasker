e56b30e17e01ffc4dc1058a8c70b68f0be0b15de
Vulpesvulpes825 <vulpesvulpes825@gmail.com>
2024-03-18 11:46:21 -0700

    fix(jwtservice): jwt token now expire after 24 hours

--- backend/src/main/kotlin/org/junyinchen/collabtaskerbackend/services/JwtService.java
+++ backend/src/main/kotlin/org/junyinchen/collabtaskerbackend/services/JwtService.java
@@ -54,7 +54,7 @@ public class JwtService {
                 .and()
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSignInKey(), Jwts.SIG.HS256)
                 .compact();
     }
