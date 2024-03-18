a5963f4c98cbb270d410c5a8c6a7c45e374de29f
VulpesVulpes825 <vulpesvulpes825@gmail.com>
2024-03-18 09:35:30 -0700

    feat(frontend): switch to shadcn/ui (#41)
    BREAKING CHANGE: Ant Design usage has been removed

--- frontend/src/component/todo-board.tsx
+++ frontend/src/component/todo-board.tsx
@@ -1,10 +1,9 @@
-import { Space } from "antd";
 import TodoItem from "./todo-item.tsx";
 
 export default function TodoBoard() {
   return (
-    <Space direction="vertical" size={16}>
+    <>
       <TodoItem></TodoItem>
-    </Space>
+    </>
   );
 }
