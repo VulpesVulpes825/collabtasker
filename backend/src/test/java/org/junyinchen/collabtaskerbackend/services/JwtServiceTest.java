420ba31d84e89011269cf76d9e00d53d6b95414b
Vulpesvulpes825 <vulpesvulpes825@gmail.com>
2024-03-20 13:53:07 -0700

    test(JwtServiceTest): mock userService

--- backend/src/test/java/org/junyinchen/collabtaskerbackend/services/JwtServiceTest.java
+++ backend/src/test/java/org/junyinchen/collabtaskerbackend/services/JwtServiceTest.java
@@ -1,24 +1,32 @@
 package org.junyinchen.collabtaskerbackend.services;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junyinchen.collabtaskerbackend.models.User;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class JwtServiceTest {
+    final String username = "user";
+    User user;
     @InjectMocks private JwtService jwtService;
-
     @Mock private UserDetails userDetails;
+    @Mock private UserService userService;
 
     @BeforeEach
     public void setup() {
-        when(userDetails.getUsername()).thenReturn("user");
+        when(userDetails.getUsername()).thenReturn(username);
+        when(userService.getUser(anyString()))
+                .thenReturn(Optional.ofNullable(User.builder().username(username).build()));
     }
 
     @Test
