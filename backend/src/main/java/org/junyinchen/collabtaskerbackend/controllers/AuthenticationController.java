42ad2b1170ea04d8a00885ccdcf075d8cc20849c
VulpesVulpes825 <vulpesvulpes825@gmail.com>
2024-03-20 13:55:18 -0700

    feat(AuthenticationController): add JWT refresh method (#75)

--- backend/src/main/java/org/junyinchen/collabtaskerbackend/controllers/AuthenticationController.java
+++ backend/src/main/java/org/junyinchen/collabtaskerbackend/controllers/AuthenticationController.java
@@ -1,23 +1,26 @@
 package org.junyinchen.collabtaskerbackend.controllers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import org.junyinchen.collabtaskerbackend.models.AuthenticationRequest;
 import org.junyinchen.collabtaskerbackend.models.AuthenticationResponse;
 import org.junyinchen.collabtaskerbackend.models.RegisterRequest;
+import org.junyinchen.collabtaskerbackend.models.User;
 import org.junyinchen.collabtaskerbackend.services.AuthenticationService;
+import org.junyinchen.collabtaskerbackend.services.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthenticationController {
 
     private final AuthenticationService service;
+    private final UserService userService;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
@@ -28,5 +31,13 @@ public class AuthenticationController {
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @GetMapping("/refresh")
+    public ResponseEntity<AuthenticationResponse> refresh() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("User {} is trying to refresh token", username);
+        User user = userService.getUser(username).orElseThrow();
+        return ResponseEntity.ok(service.refresh(user));
     }
 }
